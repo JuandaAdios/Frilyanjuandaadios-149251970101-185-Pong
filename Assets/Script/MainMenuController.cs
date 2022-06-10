44e3a6f3aa2d097f2d4033701783f83176b756a3
JuandaAdios <fj.adios@gmail.com>
2022-06-10 16:55:36 +0700

    BuildPlayable

---
+++ Assets/Script/MainMenuController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    private Rigidbody2D rig;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OpenAuthor()
+    {
+        Debug.Log("Created By Frilyan Juanda Adios - 149251970101-185");
+    }
+
+    private void MoveObject(Vector2 movement)
+    {
+        Debug.Log("TEST: " + movement);
+        rig.velocity = movement;
+    }
+}
---
+++ Assets/Script/ScoreController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    public Text ScoreKiri;
+    public Text ScoreKanan;
+
+    public ScoreManager manager;
+
+    private void Update()
+    {
+        ScoreKiri.text = manager.leftScore.ToString();
+        ScoreKanan.text = manager.rightScore.ToString();
+    }
+
+}
